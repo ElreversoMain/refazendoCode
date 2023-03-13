38aeafc35e5c71d90b9705300f202974d21fe96e
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-03-13 16:46:05 -0300

    Astromatch

--- astromatch/src/Pages/GetAllPerfil.js
+++ astromatch/src/Pages/GetAllPerfil.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import undertail from "../fotos/undertail.png";
 import broken from "../fotos/broken.png";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const TextButton = styled.div`
   .button {
@@ -58,72 +58,128 @@ const TextButton2 = styled.div`
     color: white;
   }
 `;
-const Alinhar=styled.div`
-img{
-  width: 550px;
-  height: 500px;
-}
-`
+const Alinhar = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
 
+  img {
+    width: 550px;
+    height: 500px;
+    box-shadow: 0 2px 10px 0 rgba(117, 117, 117, 0.77);
+    border-radius: 5px;
+    border-style: solid;
+  }
+`;
 
-  
+const Nome = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 10px 0 rgba(117, 117, 117, 0.77);
+  border-radius: 5px;
+  border-style: inset;
+`;
+const AlinharBUtton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  justify-content: space-evenly;
+`;
+const Found = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 450px;
 
-const ShowPerfil=()=>{
-    const [getProfile,setStateGetProfile]=useState({})
-    const getProfilee=()=>{
-        axios
-        .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/person")
-        .then((res)=>{
-            setStateGetProfile(res.data.profile)
-        }).catch((err)=>{
-            alert("Erro Na API")
-        })
+  button {
+    background-color: #4caf50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    background-color: white;
+    color: black;
+    border: 2px solid #4caf50;
+    background-color: #4caf50;
+    color: white;
+  }
+`;
 
-       
-    }
-     const ChooseShowProfile=(boleano)=>{
-        const body={
-            id:getProfile.id,
-            choice:boleano
-        }
-        axios
-        .post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/choose-person",
+const ShowPerfil = () => {
+  const [getProfile, setStateGetProfile] = useState({});
+  const getProfilee = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/person"
+      )
+      .then((res) => {
+        setStateGetProfile(res.data.profile);
+      })
+      .catch((err) => {
+        alert("Erro Na API");
+      });
+  };
+  const ChooseShowProfile = (boleano) => {
+    const body = {
+      id: getProfile.id,
+      choice: boleano,
+    };
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/choose-person",
         body
-        )
-        .then((res)=>{
-            getProfilee();
-        }).catch((err)=>{
-            alert("erro")
-        })
-     }
-    
-    useEffect(()=>{
+      )
+      .then((res) => {
         getProfilee();
-    },[])
+      })
+      .catch((err) => {
+        alert("erro");
+      });
+  };
 
-    let navigate=useNavigate()
-    return (
-        
-        <div>
-          <button onClick={()=>navigate("/YouChoice")}>Voltar</button>
-            <Alinhar>
-                <img src={getProfile.photo}></img>
-                <div>{getProfile.name}</div>
-                <div>{getProfile.bio}</div>
-            </Alinhar>
-            <TextButton>
-            <button class="button button1" onClick={()=>ChooseShowProfile(true)}>
-              {" "}
-              <img src={undertail} height={20} width={20}></img>
-            </button>
-          </TextButton>
-          <TextButton2>
-            <button class="button button2" onClick={()=>ChooseShowProfile(false)}>
-              <img src={broken} height={35} width={35}></img>
-            </button>
-          </TextButton2>
-        </div>
-    )
+  useEffect(() => {
+    getProfilee();
+  }, []);
 
-}
-export default ShowPerfil
+  let navigate = useNavigate();
+  return (
+    <div>
+      <Found>
+        <button onClick={() => navigate("/YouChoice")}>Matches</button>
+      </Found>
+
+      <Alinhar>
+        <img src={getProfile.photo}></img>
+        <Nome>{getProfile.name}</Nome>
+        <Nome>{getProfile.bio}</Nome>
+      </Alinhar>
+      <AlinharBUtton>
+        <TextButton>
+          <button
+            class="button button1"
+            onClick={() => ChooseShowProfile(true)}
+          >
+            {" "}
+            <img src={undertail} height={20} width={20}></img>
+          </button>
+        </TextButton>
+        <TextButton2>
+          <button
+            class="button button2"
+            onClick={() => ChooseShowProfile(false)}
+          >
+            <img src={broken} height={20} width={20}></img>
+          </button>
+        </TextButton2>
+      </AlinharBUtton>
+    </div>
+  );
+};
+export default ShowPerfil;
--- astromatch/src/Pages/GetAllYouChoice.js
+++ astromatch/src/Pages/GetAllYouChoice.js
@@ -3,85 +3,119 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const Caixinha=styled.div`
+const Caixinha = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   row-gap: 20px;
   column-gap: 20px;
   margin: 20px;
- 
-img{
-    box-shadow: 0 2px 10px 0 rgba(117,117,117,0.77);
-	border-radius: 5px;
-	
-	
-	
+  border-style: inset;
 
-width: 100px;
-height: 100px;
+  img {
+    box-shadow: 0 2px 10px 0 rgba(117, 117, 117, 0.77);
+    border-radius: 5px;
+    width: 100px;
+    height: 100px;
+    border-style: groove;
+  }
+`;
 
+const TextButton = styled.div`
+ button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
 }
+`;
+const TextButton2=styled.div`
+ background-color:#e7e7e7;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  color: black;
 `
 
 
 
+
 const ShowPerfill = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+};
+
+const YouChoice = () => {
+  const [receiv, SetReceiv] = useState([]);
+  const YouChoiceThis = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/matches"
+      )
+      .then((res) => {
+        SetReceiv(res.data.matches);
+      })
+      .catch((err) => {
+        alert("Erro no Aceitar Matches");
+      });
+  };
+  useEffect(() => {
+    YouChoiceThis();
+  }, []);
+
+  const Limpar = () => {
+    axios
+      .put(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/clear"
+      )
+      .then((res) => {
+        alert("Matches limpos");
+      })
+      .catch((err) => {
+        alert("erro no Button Clear");
+      });
   };
 
-const YouChoice=()=>{
-    const[receiv,SetReceiv]=useState([])
-
-    const YouChoiceThis=()=>{
-        axios
-        .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/matches")
-        .then((res)=>{
-            SetReceiv(res.data.matches)
-        })
-        .catch((err)=>{
-            alert("Erro no Aceitar Matches")
-        })
-
-        
-
-    }
-    useEffect(()=>{
-            YouChoiceThis();
-        },[])
-
+  
+  const listarenderizar = receiv.map((profile) => {
     
-        const Limpar=()=>{
-            axios
-            .put("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/felipe-oliveira-hooks/clear")
-            .then((res)=>{
-                alert("Matches limpos")
-            })
-            .catch((err)=>{
-                alert("erro no Button Clear")
-            })
-        }
-       
-
-    const listarenderizar=receiv.map((profile)=>{
-        return<div >
-            <Caixinha key={receiv.key}>
-            <img src={profile.photo} alt=""></img>
-            <h1>{profile.name}</h1>
-            <h1>{profile.age}</h1>
-            </Caixinha>
-        </div>
-    })
-    let navigate=useNavigate()
-    return(
-        <>
-        <div><button onClick={()=>navigate("/")}>Voltar</button></div>
-        <div>
-            <div> {listarenderizar}</div>
-            
-            <div><button onClick={()=>Limpar()}>Limpar</button></div>
-        </div>
+    return (
         
-        </>
-    )
-}
-export default YouChoice
+      <div>
+        <Caixinha key={profile.id}>
+          <img src={profile.photo} alt=""></img>
+          <h1>{profile.name}</h1>
+          <h1>{profile.age}</h1>
+          <h4>{profile.bio}</h4>
+    
+        </Caixinha>
+      </div>
+    );
+  });
+  let navigate = useNavigate();
+  return (
+    <>
+      <TextButton>
+        <button onClick={() => navigate("/")}>Voltar</button>
+      </TextButton>
+      <div>
+        <div> {listarenderizar}</div>
+
+        <div>
+          <TextButton2   onClick={() => Limpar()}>Limpar</TextButton2>
+        </div>
+      </div>
+    </>
+  );
+};
+export default YouChoice;
