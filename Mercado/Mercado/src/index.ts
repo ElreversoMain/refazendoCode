a22cf88a7cce82e2ec2b994e250d52d5a87f7dba
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-04-20 22:40:38 -0300

    mercado70%

--- Mercado/Mercado/src/index.ts
+++ Mercado/Mercado/src/index.ts
@@ -2,9 +2,11 @@ import app from "./app"
 import {PutProduct} from "./Endpoint/productCreate"
 import { information } from "./Endpoint/allProducts"
 import {searchProduct} from "./Endpoint/SelectforID"
+import {searchProductName} from "./Endpoint/SelectForName"
 
 app.post("/PutProduct",PutProduct)
 app.get("/information",information)
 app.get("/searchProduct",searchProduct)
+app.get("/searchProductName",searchProductName)
 
 
