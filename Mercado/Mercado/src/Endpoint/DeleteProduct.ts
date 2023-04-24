5c5d1f3b611369d2e865e1c5d83ceeeaac83e32f
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-04-24 17:21:41 -0300

    Mercado70%

---
+++ Mercado/Mercado/src/Endpoint/DeleteProduct.ts
@@ -0,0 +1,21 @@
+import {Request,Response} from "express"
+import connection from "../BaseDatabase"
+import {Buying} from "../Types/Comprar"
+
+export const DeleteProductId=async(req:Request,res:Response)=>{
+    let ErrorCode=400
+    try{
+        let id=req.query.id as string
+
+        const deletar= await connection("Shopping")
+        .where("id","like",`%${id}%`)
+        .delete()
+
+        res.status(201).send("produto deletado")
+
+
+    }catch(error:any){
+        res.status(ErrorCode).send({message:error.message})
+
+    }
+}
--- Mercado/Mercado/src/index.ts
+++ Mercado/Mercado/src/index.ts
@@ -3,10 +3,14 @@ import {PutProduct} from "./Endpoint/productCreate"
 import { information } from "./Endpoint/allProducts"
 import {searchProduct} from "./Endpoint/SelectforID"
 import {searchProductName} from "./Endpoint/SelectForName"
+import { DeleteProductId} from "./Endpoint/DeleteProduct"
+
 
 app.post("/PutProduct",PutProduct)
 app.get("/information",information)
 app.get("/searchProduct",searchProduct)
 app.get("/searchProductName",searchProductName)
+app.delete("/DeleteProductId", DeleteProductId)
+
 
 
