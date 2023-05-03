726c762d2a370d2cce74ed96c5df4cba3a7e066f
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-05-03 14:37:00 -0300

    Back-end5%

---
+++ Back-end para Projeto Free/Back-end para Projeto Free/src/Types/ComentarioSobre.ts
@@ -0,0 +1,20 @@
+export class comentarios{
+    constructor(  
+     private name:string,
+     private hora:number,
+     private comentario:string
+    ){
+        this.name=name
+        this.hora=hora
+        this.comentario=comentario
+    }
+    public GetName(){
+        return this.name
+    }
+    public GetHora(){
+        return this.hora
+    }
+    public GetComentario(){
+        return this.comentario
+    }
+}
