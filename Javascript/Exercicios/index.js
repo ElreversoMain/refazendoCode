5e653922fec114d4f78b216ecd3a760c0497ab4b
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-05-22 17:48:13 -0300

    ExercicioJavascript

---
+++ Javascript/Exercicios/index.js
@@ -0,0 +1,100 @@
+// exercicio 1
+// const altura=prompt("digite altura")
+// const largura=prompt("digite largura")
+// const resultado=altura*largura
+// console.log(`a altura:${altura} * largura ${largura} e igual ${resultado}`)
+
+// function calcular(){
+//     const largura=prompt("digite largura")
+//     const altura=prompt("digite altura")
+//     const resultado=largura*altura
+//     console.log(resultado)
+// }
+// calcular()
+
+// exercicio 2
+// const digite=Number(prompt("digite ano atual"))
+// const digite2=Number(prompt("digite ano do seu nascimento"))
+// const calculo=digite-digite2
+// console.log(calculo)
+
+// function calcularIdade(){
+//     const anoAtual=Number(prompt("Digite ano atual"))
+//     const anoNascimento=Number(prompt("digite ano nascimento"))
+//     const calcular=anoAtual-anoNascimento
+//     console.log(calcular)
+// }
+// calcularIdade()
+
+// exercicio 3
+// const peso=Number(prompt("digite seu peso"))
+// const altura=Number(prompt("digite sua altura"))
+// const calculo=peso/(altura*altura)
+// console.log(calculo)
+
+// function calcularPeso(){
+//     const peso=Number(prompt("digite seu peso"))
+//     const altura=Number(prompt("digite sua altura"))
+//     const calculo=peso/(altura*altura)
+//     console.log(calculo)
+// }
+// calcularPeso()
+
+// exercicio 4
+
+// const nome=prompt("Digite seu nome")
+// const idade=prompt("digite sua idade")
+// const email=prompt("digite seu email")
+// console.log(`Meu nome é ${nome} tenho ${idade} anos , e o meu email ${email}`)
+
+// function pegarDados(){
+//     const nome=prompt("digite seu nome")
+//     const idade=Number(prompt("digite sua idade"))
+//     const email=prompt("digite seu email")
+//     console.log(`Meu nome é ${nome} tenho ${idade} anos , e o meu email ${email}`)
+// }
+// pegarDados()
+
+//exercicio 5
+// const digite=prompt("digite sua cor favorita")
+// const digite2=prompt("digite cor 2")
+// const digite3=prompt("digite cor 3")
+// const pegar=[digite,digite2,digite3]
+// console.log(pegar)
+
+// function questionColor(){
+//     const digite1=prompt("digite cor1")
+//     const digite2=prompt("digite cor 2")
+//     const digite3=prompt("digite cor 3")
+//     result=[digite1,digite2,digite3]
+//     console.log(result)
+//     console.log(result[0])
+//     console.log(result[1])
+//     console.log(result[2])
+// }
+// questionColor()
+
+// exercicio 6
+// const digite=prompt("digite algo")
+// console.log(digite.toUpperCase())
+
+// function texteUpper(){
+//     const digite=prompt("digite algo")
+//     console.log(digite.toUpperCase())
+//     console.log(digite.toLowerCase())
+// }
+// texteUpper()
+
+// exercicio 7
+// const valor=Number(prompt("digite valor do espetaculo"))
+// const ingresso=Number(prompt(" valor do ingresso"))
+// const resultado=valor/ingresso
+// console.log(resultado)
+
+// function calcularEspetaculo(){
+//     const valor=Number(prompt("digite valor do espetaculo "))
+//     const ingresso=Number(prompt("valor do ingresso"))
+//     resultado=(valor/ingresso)
+//     console.log(resultado)
+// }
+// calcularEspetaculo()
