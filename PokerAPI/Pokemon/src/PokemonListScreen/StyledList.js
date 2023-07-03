addc9ad00423c8bed939da7fdcb224dc69ff51f5
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-07-03 17:54:05 -0300

    PokerAPI

---
+++ PokerAPI/Pokemon/src/PokemonListScreen/StyledList.js
@@ -0,0 +1,37 @@
+import styled from "styled-components"
+
+
+export const Container=styled.div`
+ display: grid;
+ justify-content: space-around;
+ flex-wrap: nowrap;
+ justify-content: center;
+ float:left;
+ justify-content: space-between;
+ row-gap: 20px;
+ column-gap: 20px;
+ margin: 20px;
+ width: 33,33%;
+ padding: 5px;
+ 
+`
+export const PokerName=styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const PokerImage=styled.div`
+background-color: black;
+border-radius: 500px;
+align-items: center;
+justify-content: center;
+padding-left: 25px;
+
+`
+
+
+
+
+  
+
+
