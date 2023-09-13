239ee0559cbd6292f27163af730bc6c1746db9e5
ElreversoMain <felipe123oliveirasantos@gmail.com>
2023-09-13 20:02:08 -0300

    FutureEats

--- Ifood/ifood/src/Pages/UpdateProfile/UpdateProfile.js
+++ Ifood/ifood/src/Pages/UpdateProfile/UpdateProfile.js
@@ -9,24 +9,62 @@ const UpdateProfile = () => {
   const [newCPF, setStateNewCPF] = useState("");
 
   const updateProfile = (event) => {
-    event.preventDefault()
-    const headers = {headers : {auth : localStorage.getItem("token")}}
-
+      setStateNewName(event.target.value);
+    };
+    const updateEmail = (event) => {
+      setStateNewEmail(event.target.value);
+    };
+    const updateCpf = (event) => {
+      setStateNewCPF(event.target.value);
+    };
+  
+  const Update=()=>{
+    const URL=`${BASE_URL}/profile`;
+    const header={
+      headers:{
+        auth:window.localStorage.getItem("token")
+      },
+    };
+    const body={
+      name:newName,
+      email:newEmail,
+      cpf:newCPF
+    };
     axios
-        .put(`${BASE_URL}profile`, form, headers )
-        .then(() => {
-            alertSuccess("Conta atualizada com sucesso!")
-        })
+    .put(URL,body,header)
+    .then((res)=>{
+      alert(`Sucesso ${newName}`)
+      console.log(res)
+    })
+    .catch((erro)=>{
+      alert("erro no Update")
+    })
+  }
+  return <div>
+    <div><TextField
+    name="NewName"
+    placeholder="Escolha um novo Nome"
+    label="*Nome"
+    onChange={(event)=>updateProfile(event)}
+    value={newName}
+    /></div>
 
-        .catch((error) => {
-            alertError("Algo de errado ocorreu :(")
-            console.log(error)
-        })
-}
-  return (
     <div>
-    
+      <TextField
+      name="NewEmail"
+      placeholder="Escolha um novo Email"
+      label="*Email"
+      onChange={(event)=>updateEmail(event)}/>
     </div>
-  );
+    <div>
+      <TextField
+      name="NewCpf"
+      placeholder="Escolha um CPF"
+      label="*CPF"
+      onChange={(event)=>updateCpf(event)}/>
+    </div>
+    <button onClick={()=>Update()}></button>
+
+  </div>;
 };
 export default UpdateProfile;
