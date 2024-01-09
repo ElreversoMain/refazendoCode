fac613c1b150eeb2298ad47fcea461c367c29758
ElreversoMain <felipe123oliveirasantos@gmail.com>
2024-01-09 19:40:51 -0300

    Colocando

---
+++ Loja/lojinha/src/Pages/Homer/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+
+export const Container=styled.div`
+padding-left:50px;
+display: grid;
+align-items: center;
+justify-content: center;
+`
+export const Capa=styled.div`
+
+`
